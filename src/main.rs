156b0aff3a55aa3b1baa6340acc0594f4cce2b9d
lyh <liaoyinghai@yymt.com>
2024-12-12 17:32:38 +0800

    submit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(slice_as_array)]
 #![feature(mpmc_channel)]
 #![feature(pattern)]
+#![feature(async_closure)]
 
 use std::collections::HashMap;
 use actix_web::{App, HttpRequest, HttpServer, Responder, middleware::Logger, web};
@@ -8,6 +9,7 @@ use actix_ws::Message;
 use log::log;
 use std::io::Read;
 use std::str::pattern::Pattern;
+use std::sync::Arc;
 use std::sync::mpmc::TryRecvError;
 use std::sync::mpsc::channel;
 use serde_json::{json, Value};
@@ -18,7 +20,9 @@ async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Resp
     actix_web::rt::spawn(async move {
         let (sender, receiver) = channel();
 
-        let _ = std::thread::spawn(move || {
+        let mut session_ = session.clone();
+
+        let handle = std::thread::spawn(async move || {
             println!("开始发送指令");
             let mut input_str = String::new();
             let stdin = std::io::stdin();
@@ -26,8 +30,11 @@ async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Resp
                 stdin.read_line(&mut input_str).unwrap();
 
                 match input_str {
-                   ref in_str if in_str.starts_with("e") => {
+                    ref in_str if in_str.starts_with("e") => {
                         sender.send("exit".to_string()).unwrap();
+                        unsafe {
+                            session_.close(None).await.unwrap();
+                        }
                         break;
                     }
                     _ => {
@@ -44,26 +51,19 @@ async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Resp
                 });
                 let string = serde_json::to_string(&value).unwrap();
                 // println!("string:{}", string);
-                sender.send(string).unwrap();
+                // sender.send(string).unwrap();
+                unsafe {
+                    session_.text(string).await.unwrap();
+                }
                 println!("发送指令");
             }
-        })
-        .thread();
+        });
+
+        drop(handle);
+
+
 
         while let Some(Ok(msg)) = msg_stream.recv().await {
-            match receiver.try_recv() {
-                Ok(text) => {
-                    //发送消息
-                    if text == "exit" {
-                        session.clone().close(None).await.unwrap();
-                        continue;
-                    }else{
-                        // println!("发送消息:{}", text);
-                        session.text(text).await.unwrap();
-                    }
-                }
-                Err(_) => {}
-            }
             match msg {
                 Message::Ping(bytes) => {
                     // println!("pong: {:?}", String::from_utf8(bytes.to_owned().to_vec()));
@@ -97,9 +97,9 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .route("/ws", web::get().to(ws))
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await?;
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
 
     Ok(())
 }
